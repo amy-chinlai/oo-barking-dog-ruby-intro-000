6f299b0cec8e2cf987853e26825efa41d85c7aff
Amy Chin-Lai <amy.chinlai@gmail.com>
2020-05-11 20:36:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,12 @@
 class Dog
-	def initialize(name)
-		@name=name
-	end
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
 
-	def name=(name)
-		@name=name
-	end
-
-	def name
-		@name
-	end
-
-	def bark
-		puts "woof!"
-	end
-end
+  def bark
+    puts "woof!"
+  end
+end 
